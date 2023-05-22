18347f0433ddff84261b13150fe354f9d77e4cfd
KevinJ6219 <kevinj66@nycstudents.net>
2023-05-22 09:11:57 -0400

    Added getter methods and a separateList() method to get each term in the original equation.

--- src/Derive.java
+++ src/Derive.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Derive {
+import java.util.ArrayList;
+
+public class Derive {
+
+    private String equation;
+    private ArrayList<String> separatedList;
+    public Derive(String equation) {
+        this.equation = equation;
+        separatedList = separate();
+    }
+
+    public ArrayList<String> separate() {
+        ArrayList<String> separtatedList = new ArrayList<String>();
+        String temp = equation;
+        if (equation.contains("+") || equation.contains("-") || equation.contains("*") || equation.contains("/")) {
+            for (int i = 0; i < temp.length(); i++) {
+                int index = 0;
+                if (temp.contains("+")) {
+                    index = temp.indexOf("+");
+                }
+                else if (temp.contains("-")) {
+                    index = temp.indexOf("-");
+                }
+                else if (temp.contains("*")) {
+                    index = temp.indexOf("*");
+                }
+                else if(temp.contains("/")) {
+                    index = temp.indexOf("/");
+                }
+                separatedList.add(temp.substring(0,index));
+                temp = temp.substring(index+1);
+            }
+            separatedList.add(temp);
+        }
+        for (int i = 0; i < separatedList.size(); i++) {
+            separatedList.set(i,separatedList.get(i).replaceAll("\\s", ""));
+        }
+        return separatedList;
+    }
+
+    public ArrayList<String> getSeparatedList() {
+        return separatedList;
+    }
+
+    public String getEQuation() {
+        return equation;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Derive question1 = new Derive("2x^2 + 3x + 3 + 6");
+        System.out.println(question1.separate());
+    }
 }
