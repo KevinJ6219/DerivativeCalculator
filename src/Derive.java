01c60f078bb336c759cf6ad4f8f0311650604f93
KevinJ6219 <kevinj66@nycstudents.net>
2023-05-23 09:29:08 -0400

    Finished quotient rule and product rule

--- src/Derive.java
+++ src/Derive.java
@@ -10,7 +10,7 @@ public class Derive {
     }
 
     public ArrayList<String> separate() {
-        ArrayList<String> separtatedList = new ArrayList<String>();
+        ArrayList<String> separatedList = new ArrayList<String>();
         String temp = equation;
         if (equation.contains("+") || equation.contains("-") || equation.contains("*") || equation.contains("/")) {
             for (int i = 0; i < temp.length(); i++) {
@@ -41,18 +41,39 @@ public class Derive {
     public String powerRule(String term) {
         String result = "";
         int power = 0;
-        int coefficient = 0;
+        int coefficient = Integer.parseInt(term.substring(0,term.indexOf("x")));
         if (term.contains("^")){
             power = Integer.parseInt(term.substring(term.indexOf("^") + 1));
         }
-        result =
+        int newCoefficient = coefficient * power;
+        result = "" + newCoefficient + "x" + "^" + (power - 1);
+        return result;
+    }
+
+    public String quotientRule(String fraction) {
+        String firstTerm = fraction.substring(0,fraction.indexOf("/"));
+        String secondTerm = fraction.substring(fraction.indexOf("/") + 1);
+        String result = "((" + powerRule(firstTerm) + "*" + secondTerm + ") - (" + powerRule(secondTerm) + "*" + firstTerm + ")) / (" + secondTerm + ")^2";
+        return result;
+    }
+
+    public String productRule(String function) {
+        String firstTerm = function.substring(0,function.indexOf("*")).replaceAll("\\s", "");
+        String secondTerm = function.substring(function.indexOf("*") + 1).replaceAll("\\s", "");
+        String result = "((" + powerRule(firstTerm) + "*" + secondTerm + ") + (" + powerRule(secondTerm) + "*" + firstTerm + "))";
+        return result;
+    }
+
+    public String solve() {
+        for (int i = 0; i < separatedList.size(); i = i + 2) {
+        }
+        return null;
     }
 
     public ArrayList<String> getSeparatedList() {
         return separatedList;
     }
-
-    public String getEQuation() {
+    public String getEquation() {
         return equation;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Derive question1 = new Derive("2x^2 + 3x + 3 + 6");
         System.out.println(question1.separate());
+        System.out.println(question1.powerRule("-3x^5"));
+        System.out.println(question1.quotientRule("5x^3/6x^5"));
+        System.out.println(question1.productRule("7x^8 * 8x^2"));
     }
 }
