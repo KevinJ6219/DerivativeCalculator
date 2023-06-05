0ae5b66feb5044e38d799af64816cf62a8196a7a
KevinJ6219 <kevinj66@nycstudents.et>
2023-06-05 09:08:06 -0400

    Tried implementing PEMDAS

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class  Main {
         String equation = input.nextLine();
         Derive question = new Derive(equation);
 //        System.out.println(question.getSeparatedList());
-//        System.out.println(question.getSymbols());
+        System.out.println(question.getSymbols());
         question.solve();
     }
 }
